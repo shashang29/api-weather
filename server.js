dd8143a2aa60a3c06f9f12ac0ed870ef3d2efe7e
Shashang <51124608+shashang29@users.noreply.github.com>
2020-01-30 22:39:14 +0545

    moved the api call to the backend for security purpose.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors= require('cors');
+const cors = require('cors');
 const fetch = require('node-fetch');
 
 const app = express();
@@ -9,19 +9,15 @@ app.use(cors());
 
 
 
+const apiKey = 'fb07371e9e8f6a6c53f85498e92dcea1';
+app.post('/api', (req, res) => {
 
-const apiKey = '';
-
-app.post('/api', (req, res)=>{
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.input}&units=metric&APPID=fb07371e9e8f6a6c53f85498e92dcea1`)
-    .then(data=>{
-        res.json(data);
-    })
-        .then(json => console.log(json))
-    .catch(err=> res.status(400).json('unable to work with api'))
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.input}&units=metric&APPID=${apiKey}`)
+        .then(data => data.json()).then(data => res.send(data))
+        .catch(err => res.status(400).json('unable to work with api'))
 })
 
 
-app.listen(3001, ()=>{
+app.listen(3001, () => {
     console.log('app is running on 3001')
 });
