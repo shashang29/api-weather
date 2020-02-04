f1512007243166c2aadd5981cc298c55d7562fa5
Shashang <51124608+shashang29@users.noreply.github.com>
2020-02-04 19:00:47 +0545

    minor change

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const apiKey = 'fb07371e9e8f6a6c53f85498e92dcea1';
 app.post('/api', (req, res) => {
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.input}&units=metric&APPID=${apiKey}`)
-        .then(data => data.json()).then(data => res.send(data))
-        .catch(err => res.status(400).json('unable to work with api'))
+        .then(data => data.json())
+        .then(data => res.send(data))
+        .catch(err => res.status(400).json('Unable to work with api'))
 })
 
 const port = process.env.PORT || 3001
