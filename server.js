b5798bd147957b1af20153580d9755ece748593b
Shashang <51124608+shashang29@users.noreply.github.com>
2020-02-06 11:20:05 +0545

    minor edit to the code

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const apiKey = 'fb07371e9e8f6a6c53f85498e92dcea1';
 app.post('/api', (req, res) => {
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.input}&units=metric&APPID=${apiKey}`)
-        .then(data => {res.json(data)})
+        .then(data => res.json(data))
+        .then(data => res.send(data))
         .catch(err => res.status(400).json('Unable to work with api'))
 })
 
