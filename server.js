c1239461972911655d822309672733e1f88c8b44
Shashang <51124608+shashang29@users.noreply.github.com>
2020-02-07 11:22:17 +0545

    minor edit to the code

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ app.get('/', (req,res)=>{res.send('it is working')});
 
 const apiKey = 'fb07371e9e8f6a6c53f85498e92dcea1';
 app.post('/api', (req, res) => {
-
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.input}&units=metric&APPID=${apiKey}`)
-        .then(data => res.json(data))
+        .then(data => data.json())
         .then(data => res.send(data))
         .catch(err => res.status(400).json('Unable to work with api'))
 })
 
-const port = process.env.PORT || 3005
+const port = process.env.PORT || 3008
 app.listen(port, () => console.log(`app is running on ${port}`)
 );
